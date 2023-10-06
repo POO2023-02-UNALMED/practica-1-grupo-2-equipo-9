045025fb653511c22d73201c067c629a5a986201
Ricardo Lopera <141966162+RicardoLopera@users.noreply.github.com>
2023-10-06 11:35:31 -0500

    veremos

--- src/datper/Persona.java
+++ src/datper/Persona.java
@@ -2,9 +2,9 @@ package datper;
 
 public class Persona {
 
-	public int cedula;
+	protected int cedula; //protected funciona para heredar
 	public String nombre;
-	public String direccion;
+	protected String direccion;
 		
 	public Persona(int cedula , String nombre, String direccion) {
 		this.cedula = cedula;
@@ -12,6 +12,20 @@ public class Persona {
 		this.direccion = direccion;
 	}
 	
-		
+	public int getCedula() {
+		return cedula;
+	}
+	
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
 }
 
