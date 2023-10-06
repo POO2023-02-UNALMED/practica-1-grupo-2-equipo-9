a0e4f585c44bfa1610faca3f995f409a467dd040
Ricardo Lopera <141966162+RicardoLopera@users.noreply.github.com>
2023-10-06 11:44:50 -0500

    ay mijo

---
+++ src/datper/Estudiante.java
@@ -0,0 +1,29 @@
+package datper;
+
+public class Estudiante extends Persona {
+
+	private int estrato;
+	private double promedio;
+	public long valormatricula;
+	int numerocreditosmatriculados;
+	
+	public Estudiante(int cedula, String nombre, String direccion, int estrato, double promedio, long valormatricula) {
+		super(cedula, nombre, direccion);
+		this.estrato = estrato;
+		this.promedio = promedio;
+		this.valormatricula = valormatricula;
+	}
+	
+	public int getEstrato() {
+		return estrato;
+	}
+	public void setEstrato(int estrato) {
+		this.estrato = estrato;
+	}
+	public double getPromedio() {
+		return promedio;
+	}
+	
+	
+	
+}
