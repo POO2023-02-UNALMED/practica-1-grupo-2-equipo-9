034df1516afc80db6ca3f48420597623cb79ead6
Ricardo Lopera <141966162+RicardoLopera@users.noreply.github.com>
2023-10-06 11:07:44 -0500

    :100:

---
+++ datper/Persona.java
@@ -0,0 +1,15 @@
+package datper;
+
+public class Persona {
+
+	public int cedula;
+	public String nombre;
+	public String direccion;
+	
+	public Persona(int cedula , String nombre, String direccion) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.direccion = direccion;
+	}
+	
+}
